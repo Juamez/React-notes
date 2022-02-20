ff4bb818e4050d2cf2ec9a6c3a525286a6964385
Juamez <juanmendezs.a@gmail.com>
2022-02-19 21:39:51 -0500

    added services and managed db info

---
+++ src/components/Note.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Note = ({note, toggleImportance}) => {
+    const label = note.important
+      ? 'make not important' : 'make important'
+
+    return (
+      <ul>
+        <li>{note.content}</li>
+        <button onClick={toggleImportance}>{label}</button>
+      </ul>
+    )
+}
+
+export default Note
